7b0f010405d53c3528a287d907929617b017bc69
mank <opensuseuser96@web.de>
2022-12-04 16:58:05 +0100

    scpi lib working

--- fw/main.cpp
+++ fw/main.cpp
@@ -4,7 +4,7 @@
 #include "pico/stdlib.h"
 #include "pio_spi.hpp"
 
-//include "scpi/scpi.h"
+#include "scpi/scpi.h"
 
 // This program instantiates a PIO SPI with each of the four possible
 // CPOL/CPHA combinations, with the serial input and output pin mapped to the
@@ -19,7 +19,17 @@
 
 #define BUF_SIZE 150
 
-/*
+#define SCPI_ERROR_QUEUE_SIZE 17
+scpi_error_t scpi_error_queue_data[SCPI_ERROR_QUEUE_SIZE];
+
+#define SCPI_INPUT_BUFFER_LENGTH 256
+static char scpi_input_buffer[SCPI_INPUT_BUFFER_LENGTH];
+
+scpi_result_t DMM_MeasureVoltageDcQ(scpi_t * context) {
+    SCPI_ResultDouble(context, 3.14);
+    return SCPI_RES_OK;
+}
+
 scpi_command_t scpi_commands[] = {
 	{ .pattern = "*IDN?", .callback = SCPI_CoreIdnQ,},
 	{ .pattern = "*RST", .callback = SCPI_CoreRst,},
@@ -27,18 +37,20 @@ scpi_command_t scpi_commands[] = {
 	SCPI_CMD_LIST_END
 };
 
-scpi_interface_t scpi_interface = {
-	.write = myWrite,
-	.error = NULL,
-	.reset = NULL,
-	.srq = NULL,
-};
-
 size_t myWrite(scpi_t * context, const char * data, size_t len) {
     (void) context;
     return fwrite(data, 1, len, stdout);
 }
-*/
+
+scpi_interface_t scpi_interface = {
+    /*.error = */ NULL,
+    /*.write = */ myWrite,
+    /*.control = */ NULL,
+    /*.flush = */ NULL,
+    /*.reset = */ NULL,
+};
+
+scpi_t scpi_context;
 
 void test(pioSpi &spi) {
     static uint8_t txbuf[BUF_SIZE];
@@ -79,7 +91,7 @@ int main() {
     gpio_put(PIN_LED, 1);
     
     gpio_put(PIN_LED, 1);
-    printf("Hello, world!\n");
+    printf("RP2040 booting...\n");
     sleep_ms(1000);
     gpio_put(PIN_LED, 0);
     sleep_ms(1000);
@@ -90,19 +102,35 @@ int main() {
     spi0.set_mosi_pin(PIN_MOSI);
     spi0.set_baudrate(5);
     
+    SCPI_Init(&scpi_context,
+              scpi_commands,
+              &scpi_interface,
+              scpi_units_def,
+              "LTE", "probeInterface", "G2", "dev",
+              scpi_input_buffer, SCPI_INPUT_BUFFER_LENGTH,
+              scpi_error_queue_data, SCPI_ERROR_QUEUE_SIZE
+    );
+    
     while (true) {
         for (int cpha = 0; cpha <= 1; ++cpha) {
             spi0.set_cpha(cpha);
             
             for (int cpol = 0; cpol <= 0; ++cpol) {
                 spi0.set_cpol(cpol);
-                printf("CPHA = %d, CPOL = %d\n", cpha, cpol);
+                //printf("CPHA = %d, CPOL = %d\n", cpha, cpol);
                 
                 //spi0.enable(true);
-                test(spi0);
+                //test(spi0);
                 //spi0.enable(false);
+                
+                char in[40];
+                if(fgets(in, 40, stdin) != NULL){
+                    //printf("%s", in);
+                    SCPI_Input(&scpi_context, in, strlen(in));
+                }
+                //printf("ping\n");
 
-                //sleep_ms(250);
+                sleep_ms(2000);
             }
         }
     }
